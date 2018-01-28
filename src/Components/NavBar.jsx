6686910933c3e7d49163fe837c85ed5f185011f5
Jonathan <test@Johns-test-mac.wireless.rit.edu>
2018-01-27 19:09:26 -0500

    Initial commit

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+class NavBar extends Component {
+  render() {
+    return (
+      <div className="Name">
+        Jonathan Wong
+      </div>
+    )
+  }
+}
+export default NavBar;
